fd529ca64b46c94920dfce7d44e7a4d75414f31d
Unknown <alfian853@gmail.com>
2018-11-23 23:56:40 +0700

    add physijs, implement gravity, detect collision

--- helix.js
+++ helix.js
@@ -1,36 +1,57 @@
 class Helix{
 
-    constructor() {
+    constructor(scene) {
+        this.scene = scene;
         this.helix = new THREE.Group();
         this.gFloors = new THREE.Group();
         this.changeFloor = false;
         this.floors = [];
 
+        var geometry = new THREE.SphereGeometry(0.3, 15, 15);
+        var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+        this.ball = new Physijs.SphereMesh( geometry, material );
+        
+        this.ball.translateZ(2);
+        this.ball.translateY(4);
+        this.ball.isFall = true;
+        this.ball.height = 0;
+        this.ball.addEventListener('collision',function(other_object, relative_velocity, relative_rotation,
+            contact_normal){
+            //collided
+        });
+        this.helix.add(this.ball);
+        scene.add(this.ball);
+
+
         var mainCylinderGeom = new THREE.CylinderGeometry(0.5, 0.5, 16, 16, 1,true);
         var meshMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
-        var mainCylinder = new THREE.Mesh(mainCylinderGeom,meshMaterial);
-
+        var mainCylinder = new Physijs.CylinderMesh(mainCylinderGeom,meshMaterial,0);
         this.helix.add(mainCylinder);
+        scene.add(mainCylinder);
 
         var floor = this.getRandomDisc();
         floor.translateY(1.5);
         this.gFloors.add(floor);
         this.floors.push(floor);
 
+        scene.add(floor);
+
         floor = this.getRandomDisc();
         floor.translateY(-3);
         this.gFloors.add(floor);
         this.floors.push(floor);
+        scene.add(floor);
+
 
         floor = this.getRandomDisc();
         floor.translateY(-7.5);
         this.gFloors.add(floor);
         this.floors.push(floor);
-
+        scene.add(floor);
 
         this.helix.add(this.gFloors);
-
     }
+
     getRandomFloat(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     }
@@ -38,24 +59,31 @@ class Helix{
     //private
     getRandomDisc(){
         var floorGeom = new THREE.CylinderGeometry(2, 2, 0.5, 20, 1
-            ,false,this.getRandomFloat(0,Math.PI*2),Math.PI*1.6
+            ,false,this.getRandomFloat(0,Math.PI*2),Math.PI*1.5
         );
         var floorMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
-        var floor = new THREE.Mesh(floorGeom,floorMaterial)
+        var floor = new Physijs.CylinderMesh(floorGeom,floorMaterial,0)
         return floor;
     }
 
-    getObject(){
-        return this.helix;
-    }
-
     rotate(clockwise){
-        this.gFloors.rotateY(((clockwise)?-1:1)*0.2);
+        for(let i=0;i<3;i++){
+            let x = this.floors[i];
+            x.__dirtyRotation = true;
+            this.floors[i].rotateY(((clockwise)?-1:1)*0.2);
+        }
     }
 
     //private
     isCollide(){
         //not implemented yet
+        var vt = this.ball.geometry.vertices
+        var len = vt.length;
+        for(let i=0; i < len; i++){
+            
+        }
+        
+        return false;
     }
 
     //private
@@ -64,14 +92,7 @@ class Helix{
     }
 
     update(){
-        if(this.isCollide()){
-            if(this.isPassedTopFloor()){
-                
-            }
-            else{
-
-            }
-        }
+        this.scene.simulate();
     }
 
 
